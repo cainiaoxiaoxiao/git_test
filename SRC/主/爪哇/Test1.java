5baebbe76f36a0eb3e8f509ec8f0c1c4ee180858
zjh <651831787@qq.com>
2018-10-19 16:42:39 +0800

    这是第二次提交

--- SRC/主/爪哇/Test1.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by no on 2018/10/19.
- */
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.println("aaaa");
-        System.out.println("幺幺");
-    }
-}
